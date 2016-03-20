f95ae6ff5d1a34215ba93ae5f61a7609d1945275
arturschuch <arturschuch@hotmail.com>
2016-03-20 14:35:04 -0300

    Added dependencies to rest interface

--- project.clj
+++ project.clj
@@ -1,12 +1,16 @@
 (defproject reward-system "0.1.0-SNAPSHOT"
   :description "REST service for documents"
   :url "https://github.com/arturschuch"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-               [compojure "1.1.1"]
-               [ring/ring-json "0.1.2"]
-               [c3p0/c3p0 "0.9.1.2"]
-               [cheshire "4.0.3"]]
-  :plugins [[lein-ring "0.7.3"]]
-  :ring {:handler clojure-rest.handler/app}
-  :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+                 [ring/ring-core "1.1.0"]
+                 [ring/ring-devel "1.1.0"]
+                 [ring/ring-jetty-adapter "1.1.0"]
+                 [ring-json-params "0.1.3"]
+                 [compojure "1.1.0"]
+                 [clj-http "0.5.2"]
+                 [org.clojure/tools.nrepl "0.2.0-beta9"]
+                 [hiccup "1.0.1"]]
+  :dev-dependencies [[lein-ring "0.9.7"]]
+  :main com.rewards-system.main)
